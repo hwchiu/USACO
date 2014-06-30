150ca7751532b24fce5bd1054bc819aa5cd0d037
hwchiu <sppsorrg@gmail.com>
2014-06-30 13:08:07 +0800

    modify friday

--- src/Friday the Thirteenth/friday.cpp
+++ src/Friday the Thirteenth/friday.cpp
@@ -16,35 +16,28 @@ void addDays(int& days,int add){
 int main() {
     ofstream fout ("friday.out");
     ifstream fin ("friday.in");
-	int days[11]={3,0,3,2,3,2,3,3,2,3,2};
-	int ans[7]={0};
+	int days[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+	int ans[7]={0}; //0 for saturday, 1 for sunday, 2 for monday, 6 for friday.
 	int years;
 	int day = 0;
 	int leap;
 	fin >> years;
 	for(int i=0;i<years;++i){
 		leap = 0;
-		//for Jan
-		//1/1~1/13 , (13-1)%7 = 5;
-		addDays(day,5);
-		ans[day]++;
 		if( ( 0 == i%4 && 0 !=i%100) || i==100) //N=0~400 --> 1900~2300, only 2000 need to watch out. 2000-1900 = 100,
 			leap = 1;
-		for(int j=0; j<11;++j){
-			addDays(day,days[j]);
-			if( 1 == j && leap)
-				addDays(day,1);
+		for(int j=0; j<12;++j){ 
 			ans[day]++;
+			addDays(day,days[j]);
+			if(j==1 && leap)
+				addDays(day,1);
 		}
-		//compute the rest Dec
-		addDays(day,(31-13+1));
 	}
-	fout<<ans[5]<<" ";
-	fout<<ans[6]<<" ";
-	fout<<ans[0]<<" ";
-	fout<<ans[1]<<" ";
-	fout<<ans[2]<<" ";
-	fout<<ans[3]<<" ";
-	fout<<ans[4]<<endl;
+	for(int i=0;i<7;++i){
+		if(i)
+			fout<<" ";
+		fout<<ans[i];
+	}
+	fout<<endl;
     return 0;
 }
