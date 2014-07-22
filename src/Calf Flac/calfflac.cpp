63049b8e222efc1c8ee1a1226501e98e2651e539
hwchiu <sppsorrg@gmail.com>
2014-07-23 00:02:59 +0800

    speed up but not the fastest algorithm, we can search the only alpha string to reduce the unnecessary operations

--- src/Calf Flac/calfflac.cpp
+++ src/Calf Flac/calfflac.cpp
@@ -8,10 +8,14 @@ LANG: C++
 #include <string>
 using namespace std;
 void checkPalindrome(string text,int lIdx,int rIdx,int *lAns,int *rAns){
+	while(rIdx < text.size()-1 && !isalpha(text[rIdx])) rIdx++;
+	if(text[lIdx+1] == text[rIdx]){
+		rIdx++;
+	}
 	while(lIdx >= 0 && rIdx < text.size()){
 		while(lIdx >=0 && isalpha(text[lIdx])==0) lIdx--;
 		while(rIdx <text.size() && isalpha(text[rIdx])==0) rIdx++;
-		if(toupper(text[lIdx])==toupper(text[rIdx])){
+		if(text[lIdx]==text[rIdx]){
 			if( (rIdx-lIdx) > (*rAns-*lAns)){
 				*rAns = rIdx;
 				*lAns = lIdx;
@@ -28,6 +32,7 @@ int main() {
     ifstream fin ("calfflac.in");
 	string input;
 	string text;
+	string tmp;
 	int lIdx,rIdx;
 	int lAns=0,rAns=0;
 	int count=0;
@@ -35,9 +40,16 @@ int main() {
 		text.append(input);
 		text.append("\n");
 	}
+	tmp = text;
+	for(int i=0;i<tmp.size();i++){
+		if(isalpha(tmp[i]))
+			tmp[i] = toupper(tmp[i]);
+	}
+		
 	for(int i=1;i<text.size()-1;i++){
-		checkPalindrome(text,i,i+1,&lAns,&rAns);
-		checkPalindrome(text,i-1,i+1,&lAns,&rAns);
+		if(!isalpha(text[i]))
+				continue;
+		checkPalindrome(tmp,i-1,i+1,&lAns,&rAns);
 	}
 	for(int i=lAns;i<=rAns;++i){
 		if(isalpha(text[i])!=0)
